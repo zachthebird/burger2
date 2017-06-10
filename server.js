635f9d4cb5e7fb8c5ffc351b399fb53480bfad9a
Zach Bird <zachthebird@gmail.com>
2017-06-10 01:04:32 -0500

    server stuff

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(process.cwd() + "/public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var db = require('./models');
